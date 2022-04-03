341e93532cc6a6af00040c2a3228f47174f9c348
infiniteXyy <xuyiyangwing@gmail.com>
2022-04-03 19:21:52 +0800

    fix build scripts

--- src/constants/default-store.ts
+++ src/constants/default-store.ts
@@ -1,4 +1,4 @@
-import styleContent from './default-style-content.css?raw'
+import styleContent from './default-style-content.windi?raw'
 import resumeContent from './default-resume-content.md?raw'
 
 export const defaultStore = {
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
 import Icons from 'unplugin-icons/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
